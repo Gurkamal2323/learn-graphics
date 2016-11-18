d10236954e1c0c46f2a91adcb7b9859c04f597c7
Gurkamal Singh <gurkamal2323@gmail.com>
2016-11-18 19:05:16 +0530

    added first program

--- First_Program/main.cpp
+++ First_Program/main.cpp
@@ -12,11 +12,11 @@ void display() {
 
    // Draw a Red 1x1 Square centered at origin
    glBegin(GL_QUADS);              // Each set of 4 vertices form a quad
-      glColor3f(0.0f, 1.0f, 0.0f); // Red
-      glVertex2f(-0.5f, -0.5f);    // x, y
-      glVertex2f( 0.5f, -0.5f);
-      glVertex2f( 0.5f,  0.5f);
-      glVertex2f(-0.5f,  0.5f);
+     glColor3f(0.0f, 1.0f, 0.0f); // Red
+     glVertex2f(-0.5f, -0.5f);    // x, y
+     glVertex2f( 0.5f, -0.5f);
+     glVertex2f( 0.5f,  0.5f);
+     glVertex2f(-0.5f,  0.5f);
    glEnd();
 
    glFlush();  // Render now
